b3c0720694b29eb162378d93d292e6565169002e
John Oerter <joerter@gmail.com>
2014-03-22 13:30:35 -0500

    Created factory for handling server requests

--- src/app.js
+++ src/app.js
@@ -1,8 +1,9 @@
 // create angular module for dashApp
-var dashApp = angular.module('dashApp', ['ngRoute']);
+var dashApp = angular.module('dashApp', ['ngRoute', 'ngResource']);
 
 // manage routing and controllers
-dashApp.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+dashApp.config(['$routeProvider', '$locationProvider', '$httpProvider', function($routeProvider, $locationProvider, $httpProvider) {
+    // set up routes
     $routeProvider
         .when('/', {
             controller: 'listCtrl',
@@ -13,13 +14,31 @@ dashApp.config(['$routeProvider', '$locationProvider', function($routeProvider, 
             templateUrl: 'partials/docview.html'
         })
         .otherwise({ redirectTo: '/' });
+
+    // send requests with appropriate CORS headers for the whole app
+    $httpProvider.defaults.useXDomain = true;
+    delete $httpProvider.defaults.headers.common['X-Requested-With'];
 }]);
 
-// listCtrl
-dashApp.controller('listCtrl', ['$scope','$http', function($scope, $http) {
-    $http.get('../spdx.json').success( function(data, status, headers, config) {
-        $scope.docs = data;
+// spdxDoc factory for managing getting spdx docs from the server
+dashApp.factory('spdxDoc', ['$resource', function($resource) {
+    return $resource('spdxdev.ist.unomaha.edu:3000/api/spdx', {}, {
+        query: {method:'GET', isArray:true}
     });
+}]);
+
+//***************************************************************************
+//Controllers
+//
+//listCtrl - Controller for dashboard home, gets a list of all SPDX Docs and
+//           displays it
+//docCtrl - Controller for the single doc view, displays a SPDX doc in html
+//          format
+//***************************************************************************
+
+// listCtrl
+dashApp.controller('listCtrl', ['$scope','spdxDoc', function($scope, spdxDoc) {
+   $scope.docs = spdxDoc.query(); 
 }]);
 
 // docCtrl
